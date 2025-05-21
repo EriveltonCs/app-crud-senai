f203ee827fc6a2ffea61921613335dc58e0daa30
Programador Mobile <programador@sfiec.org.br>
2025-05-20 21:01:18 -0300

    subindo arquivos

--- backend-aula/index.js
+++ backend-aula/index.js
@@ -2,11 +2,50 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const sqlite3 = require(sqlite3).verbose(); // Banco sqlite
 
+// Criando o servidor
 const app = express();
 
+// Configura o servidor
 app.use(cors());
 app.use(bodyParser.json());
+
+const db = new sqlite3.Database('./tarefas.db', (err) =>
+{
+    if (err) {
+        console.error("Erro ao conectar ao Sqlite:", err);
+    } else {
+        console.error("Conectado ao Sqlite:");
+    }
+}
+);
+
+// Criando tabelas
+db.serialize(() =>
+    {
+        db.run(
+            'CREATE TABLE IF NOT EXISTS usuarios(id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT UNIQUE NOT NULL, password TEXT NOT NULL)'
+        );
+        db.run(
+            'CREATE TABLE IF NOT EXISTS tarefas(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT NOT NULL, userId INTEGER NOT NULL, FOREIGN KEY (userId) REFERENCES usuarios(id))'
+        );
+    }
+);
+
+
+// Fechando banco de dados ao encerrar o servidor
+process.on('SIGINT', () => {
+    db.close((err) => {
+        if (err) {
+            console.error('Erro ao fechar o SQlite');
+        } else {
+            console.error('SQlite fechado');
+            process.exit(0);
+        }
+    });
+});
+
 
 
 // Definir uma rota para teste
